fea58a97dc0bcb14c63e8ff9d20b5dd6db943741
AMekid918 <49527187+AMekid918@users.noreply.github.com>
2021-12-16 19:51:08 +0000

    final controller fixes

--- app/Models/User.php
+++ app/Models/User.php
@@ -64,4 +64,8 @@ class User extends Authenticatable
         return $this->hasMany(Like::class);
     }
 
+    public function username(){
+        return $this->hasOne(Username::class);
+    }
+
 }
